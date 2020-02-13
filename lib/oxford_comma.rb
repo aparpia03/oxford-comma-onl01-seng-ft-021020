b9710bec25a65fbd0236877d09d5e953ff794ec6
Abbas Parpia <aparpia03@gmail.com>
2020-02-13 23:26:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array_of_strings)
   if array_of_strings.length == 1
       return array_of_strings.length[0]
   elseif array_of_strings.length == 2 
-    return array_of_strings.join('and')
+    return array_of_strings.join(' and ')
   else
-    puts array_of_strings[0..-2].join(' , ') + " ,and " + array_of_strings[-1]
+    return array_of_strings[0..-2].join(' , ') + " ,and " + array_of_strings[-1]
 end  
 end 
